39a2242db37029edbd21f7785f7b93ae2b595a15
Tucker <trblackw@ncsu.edu>
2018-08-23 13:42:12 -0400

    created base functionality for chat app & added firebase to package.json

--- App/Controllers/chat.js
+++ App/Controllers/chat.js
@@ -1,1 +1,35 @@
+//establish front-end socket connection
 const socket = io.connect("http://localhost:3000/");
+
+//DOM elements
+const userHandle = document.querySelector("#handle");
+const userMessage = document.querySelector("#message");
+const sendButton = document.querySelector("#send");
+const userOutput = document.querySelector("#output");
+const messageFeedback = document.querySelector("#feedback");
+
+//emit socket events
+//listens for message to be created then sends it to the server
+sendButton.addEventListener("click", function() {
+  socket.emit("chat", {
+    message: userMessage.value,
+    handle: userHandle.value
+  });
+});
+
+//text feedback listener
+userMessage.addEventListener("keypress", function() {
+  socket.emit("typing", userHandle.value);
+});
+
+//Listen for emit events
+socket.on("chat", function(data) {
+  messageFeedback.innerHTML = "";
+  userOutput.innerHTML += `<p><strong>${data.handle}:</strong> ${
+    data.message
+  }</p>`;
+});
+
+socket.on("typing", function(data) {
+  messageFeedback.innerHTML = `<p><em>${data}'s typing...</em></p>`;
+});
--- index.js
+++ index.js
@@ -11,17 +11,25 @@ app.set("view engine", "html");
 
 app.use("/", express.static(__dirname + "/"));
 
-app.get("/", (req, res) => {
+app.get("/", function(req, res) {
   res.render("index");
 });
 
-const server = app.listen(3000, () => {
+const server = app.listen(3000, function() {
   console.log("Server is listening on port 3000");
 });
 
 //socket config
 const io = socket(server);
 
-io.on('connection', socket => {
-   console.log('made socket connection: ', socket)
-})
+io.on("connection", function(socket) {
+  console.log("made socket connection: ", socket.id);
+  //server listens to all sockets, when it hears 'chat' message, it emits that message and its data to all other listening clients
+  socket.on("chat", function(data) {
+    io.sockets.emit("chat", data);
+  });
+   
+   socket.on('typing', function(data) {
+      socket.broadcast.emit('typing', data)
+   })
+});
